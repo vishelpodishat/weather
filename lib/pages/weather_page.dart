23673ef15b8a1a97e96a8b74727ae93b1952e9e1
vishelpodishat <alishsaideshov@gmail.com>
2024-06-28 13:36:57 +0500

    add container color

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:weather/model/forecast_day.dart';
 import 'package:weather/model/weather_responce.dart';
+import 'package:weather/resources/colors.dart';
 import 'package:weather/service/weather_service.dart';
 
 class WeatherPage extends StatefulWidget {
@@ -71,28 +72,39 @@ class _WeatherPageState extends State<WeatherPage> {
                 border: const OutlineInputBorder(),
               ),
             ),
+            const SizedBox(height: 20),
             if (_currentWeather != null) ...[
-              const SizedBox(height: 20),
-              Text(
-                _currentWeather!.name,
-                style: const TextStyle(fontSize: 32),
+              Container(
+                padding: const EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  color: AppColors.heavenlyBlue, // Use the defined color
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      _currentWeather!.name,
+                      style: const TextStyle(fontSize: 32),
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          '${_currentWeather!.main.temp}°C',
+                          style: const TextStyle(fontSize: 32),
+                        ),
+                        const SizedBox(width: 10),
+                        Image.network(
+                            'https://openweathermap.org/img/w/${_currentWeather!.weather[0].icon}.png'),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    Text('Humidity: ${_currentWeather!.main.humidity}%'),
+                    Text('Wind Speed: ${_currentWeather!.wind.speed} m/s'),
+                  ],
+                ),
               ),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    '${_currentWeather!.main.temp}°C',
-                    style: const TextStyle(fontSize: 32),
-                  ),
-                  const SizedBox(width: 10),
-                  Image.network(
-                      'https://openweathermap.org/img/w/${_currentWeather!.weather[0].icon}.png'),
-                ],
-              ),
-              const SizedBox(height: 10),
-              Text('Humidity: ${_currentWeather!.main.humidity}%'),
-              Text('Wind Speed: ${_currentWeather!.wind.speed} m/s'),
               const SizedBox(height: 20),
               const Text(
                 '3-Day Forecast',
@@ -111,18 +123,45 @@ class _WeatherPageState extends State<WeatherPage> {
     if (_weatherForecast == null) {
       return const CircularProgressIndicator();
     }
-    return Column(
-      children: _weatherForecast!.daily.map((day) {
-        String formattedDate = day.date.split(' ')[0];
 
-        return ListTile(
-          leading: Image.network(
-              'https://openweathermap.org/img/w/${day.weather[0].icon}.png'),
-          title: Text('${day.temp.day}°C'),
-          subtitle: Text(day.weather[0].description),
-          trailing: Text(formattedDate),
-        );
-      }).toList(),
+    return Expanded(
+      child: ListView(
+        children: _weatherForecast!.daily.map((day) {
+          String formattedDate = day.date.split(' ')[0];
+
+          return Container(
+            margin: const EdgeInsets.symmetric(vertical: 8.0),
+            padding: const EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              color: AppColors.forecastBlue, // Use the defined color
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  formattedDate,
+                  style: const TextStyle(fontSize: 18),
+                ),
+                Row(
+                  children: [
+                    Image.network(
+                      'https://openweathermap.org/img/w/${day.weather[0].icon}.png',
+                      width: 50,
+                      height: 50,
+                    ),
+                    const SizedBox(width: 10),
+                    Text(
+                      '${day.temp.day}°C',
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        }).toList(),
+      ),
     );
   }
 }
--- lib/resources/colors.dart
+++ lib/resources/colors.dart
@@ -1,1 +1,6 @@
+import 'package:flutter/material.dart';
 
+class AppColors {
+  static const Color heavenlyBlue = Color(0xFFB3E5FC); // Light Blue 100
+  static const Color forecastBlue = Color(0xFFE1F5FE); // Light Blue 50
+}
